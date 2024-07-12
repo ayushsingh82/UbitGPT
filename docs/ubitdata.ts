fde87ee8061b685464c5c0ddacb88978affa96d7
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-07-12 15:03:07 +0530

    ubit data

---
+++ docs/ubitdata.ts
@@ -0,0 +1,16 @@
+// data.ts
+export interface UrlData {
+    name: string;
+    url: string;
+  }
+  
+  export const urls: UrlData[] = [
+    { name: "Tokens", url: "https://ubitscan.io/tokens" },
+    { name: "Accounts", url: "https://ubitscan.io/accounts" },
+    { name: "GraphiQL", url: "https://ubitscan.io/graphiql" },
+    { name: "UbitSwap Sale", url: "https://sale.ubitswap.com/" },
+    { name: "UbitCoin", url: "https://ubitcoin.in/" },
+    { name: "UbitKart", url: "https://ubitkart.com/" },
+    { name: "UbitSwap", url: "https://ubitswap.com/" }
+  ];
+  
